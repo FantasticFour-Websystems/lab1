314329e6da727d468d8293083668ebc85e00a715
Tobey Tan <tant3@rpi.edu>
2021-10-17 10:26:39 -0400

    Lab 1
    separate repo from Lab 1 and term project repo

---
+++ repeat_code/header.php
@@ -0,0 +1,19 @@
+
+<?php
+echo "
+<div class="navbar">
+      <a href="index.html">Home</a>
+      <div class="subnav">
+        <button class="subnavbtn">Meet Our Team<i class="fa fa-chevron-circle-down"></i></button>
+        <div class="subnav-content">
+          <a href ="profiles/ngoe.html">Emily Ngo</a>
+          <a href="profiles/tant3.html">Tobey Tan</a>
+          <a href="profiles/morawn.html">Nicolas Morawski</a>
+          <a href="profiles/liuz20.html">Zijin Liu</a>
+        </div>
+      </div> 
+      <a href="contact.html">Contact Us</a>
+    </div>
+
+";
+?>
